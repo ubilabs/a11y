4de8374d6051c822370ec9594e91f9777aa79659
Leslie Lawendel <zimmermann@ubilabs.net>
2025-08-20 09:24:19 +0200

    refactor: load fonts before index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import {createRoot} from 'react-dom/client';
 
 import App from './App.tsx';
 
-import './index.css';
 import './fonts.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
