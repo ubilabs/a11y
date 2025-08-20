3d3f60d7a0218bce2cda992cd63981d50a693e29
Leslie Lawendel <zimmermann@ubilabs.net>
2025-08-20 09:11:18 +0200

    feat: add list of WCAG 2.2 AA criteria

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,632 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Accessibility Done Wrong</h1>
+
+      <section>
+        <header>
+          <h2>1.1.1 Non-Text Content</h2>
+          <h3>Level A</h3>
+          <p>
+            Provide text alternatives for non-text content that serves the same
+            purpose.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.2.1 Audio-Only and Video-Only (Prerecorded)</h2>
+          <h3>Level A</h3>
+          <p>Provide an alternative to video-only and audio-only content.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.2.2 Captions (Prerecorded)</h2>
+          <h3>Level A</h3>
+          <p>Provide captions for videos with audio.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.2.3 Audio Description or Media Alternative (Prerecorded)</h2>
+          <h3>Level A</h3>
+          <p>
+            Provide audio description or text transcript for videos with sound.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.2.4 Captions (Live)</h2>
+          <h3>Level AA</h3>
+          <p>Add captions to live videos.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.2.5 Audio Description (Pre-Recorded)</h2>
+          <h3>Level AA</h3>
+          <p>Provide audio descriptions for pre-recorded videos.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.3.1 Info and Relationships</h2>
+          <h3>Level A</h3>
+          <p>
+            Content, structure and relationships can be programmatically
+            determined.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.3.2 Meaningful Sequence</h2>
+          <h3>Level A</h3>
+          <p>Present content in a meaningful order.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.3.3 Sensory Characteristics</h2>
+          <h3>Level A</h3>
+          <p>Instructions don't rely solely on sensory characteristics.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.3.4 Orientation</h2>
+          <h3>Level AA</h3>
+          <p>Your website adapts to portrait and landscape views.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.3.5 Identify Input Purpose</h2>
+          <h3>Level AA</h3>
+          <p>
+            The purpose of input fields must be programmatically determinable.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.1 Use of Color</h2>
+          <h3>Level A</h3>
+          <p>Don't use presentation that relies solely on colour.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.2 Audio Control</h2>
+          <h3>Level A</h3>
+          <p>Don't play audio automatically.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.3 Contrast Minimum</h2>
+          <h3>Level AA</h3>
+          <p>Contrast ratio between text and background is at least 4.5:1.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.4 Resize Text</h2>
+          <h3>Level AA</h3>
+          <p>
+            Text can be resized to 200% without loss of content or function.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.5 Images of Text</h2>
+          <h3>Level AA</h3>
+          <p>Don't use images of text.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.10 Reflow</h2>
+          <h3>Level AA</h3>
+          <p>
+            Content retains meaning and function without scrolling in two
+            dimensions.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.11 Non-Text Contrast</h2>
+          <h3>Level AA</h3>
+          <p>
+            The contrast between user interface components, graphics and
+            adjacent colours is at least 3:1.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.12 Text Spacing</h2>
+          <h3>Level AA</h3>
+          <p>
+            Content and function retain meaning when users change elements of
+            text spacing.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>1.4.13 Content on Hover or Focus</h2>
+          <h3>Level AA</h3>
+          <p>
+            When hover or focus triggers content to appear, it is dismissible,
+            hoverable and persistent.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.1.1 Keyboard</h2>
+          <h3>Level A</h3>
+          <p>
+            All functionality is accessible by keyboard with no specific
+            timings.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.1.2 No keyboard Trap</h2>
+          <h3>Level A</h3>
+          <p>Users can navigate to and from all content using a keyboard.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.1.4 Character Key Shortcuts</h2>
+          <h3>Level A</h3>
+          <p>
+            Allow users to turn off or remap single-key character shortcuts.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.2.1 Timing Adjustable</h2>
+          <h3>Level A</h3>
+          <p>
+            Provide user controls to turn off, adjust or extend time limits.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.2.2 Pause, Stop, Hide</h2>
+          <h3>Level A</h3>
+          <p>
+            Provide user controls to pause, stop and hide moving and
+            auto-updating content.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.3.1 Three Flashes or Below Threshold</h2>
+          <h3>Level A</h3>
+          <p>No content flashes more than three times per second.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.1 Bypass Blocks</h2>
+          <h3>Level A</h3>
+          <p>Provide a way for users to skip repeated blocks of content.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.2 Page Titled</h2>
+          <h3>Level A</h3>
+          <p>Use helpful and clear page titles.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.3 Focus Order</h2>
+          <h3>Level A</h3>
+          <p>Components receive focus in a logical sequence.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.4 Link Purpose (In Context)</h2>
+          <h3>Level A</h3>
+          <p>Every link's purpose is clear from its text or context.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.5 Multiple Ways</h2>
+          <h3>Level AA</h3>
+          <p>Offer at least two ways to find pages on your website.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.6 Headings and Labels</h2>
+          <h3>Level AA</h3>
+          <p>Headings and labels describe topic or purpose.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.7 Focus Visible</h2>
+          <h3>Level AA</h3>
+          <p>Keyboard focus is visible when used.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.4.11 Focus Not Obscured (Minimum)</h2>
+          <h3>Level AA</h3>
+          <p>
+            When a user interface component is selected, the focus indicator
+            encompasses the visual presentation of the component, maintains a
+            contrast ratio of at least 3:1 between its pixels in focused and
+            unfocused states, and ensures a contrast ratio of at least 3:1
+            against adjacent colors.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.5.1 Pointer Gestures</h2>
+          <h3>Level A</h3>
+          <p>
+            Multi-point and path-based gestures can be operated with a single
+            pointer.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.5.2 Pointer Cancellation</h2>
+          <h3>Level A</h3>
+          <p>Functions don't complete on the down-click of a pointer.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.5.3 Label in Name</h2>
+          <h3>Level A</h3>
+          <p>
+            Where a component has a text label, the name of the component also
+            contains the text displayed.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.5.4 Motion Actuation</h2>
+          <h3>Level A</h3>
+          <p>
+            Functions operated by motion can also be operated through an
+            interface and responding to motion can be disabled.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.5.7 Dragging Movements</h2>
+          <h3>Level AA</h3>
+          <p>
+            All functionality that uses a dragging movement for operation can be
+            achieved by a single pointer without dragging, unless dragging is
+            essential or the functionality is determined by the user agent and
+            not modified by the author.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>2.5.8 Target Size (Minimum)</h2>
+          <h3>Level AA</h3>
+          <p>
+            Ensure the target of any UI element has 24 by 24 CSS PX target size
+            or there is enough spacing provided between two targets that have
+            undersize targets.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.1.1 Language of Page</h2>
+          <h3>Level A</h3>
+          <p>Each webpage has a default human language assigned.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.1.2 Language of Parts</h2>
+          <h3>Level AA</h3>
+          <p>Each part of a webpage has a default human language assigned.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.2.1 On Focus</h2>
+          <h3>Level A</h3>
+          <p>Elements do not change when they receive focus.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.2.2 On Input</h2>
+          <h3>Level A</h3>
+          <p>Elements do not change when they receive input.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.2.3 Consistent Navigation</h2>
+          <h3>Level AA</h3>
+          <p>Position menus and standard controls consistently.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.2.4 Consistent Identification</h2>
+          <h3>Level AA</h3>
+          <p>Identify components with the same function consistently.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.2.6 Consistent Help</h2>
+          <h3>Level A</h3>
+          <p>Help options are presented programmatically in the same order.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.3.1 Error Identification</h2>
+          <h3>Level A</h3>
+          <p>Identify and describe input errors for users.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.3.2 Labels or Instruction</h2>
+          <h3>Level A</h3>
+          <p>Provide labels or instructions for user input.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.3.3 Error Suggestion</h2>
+          <h3>Level AA</h3>
+          <p>Suggest corrections when users make mistakes.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.3.4 Error Prevention (Legal, Financial, Data)</h2>
+          <h3>Level AA</h3>
+          <p>
+            Check, confirm and allow reversal of pages that cause important
+            commitments.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.3.7 Redundant Entry</h2>
+          <h3>Level A</h3>
+          <p>Autofill form- fields that repeat across steps.</p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>3.3.8 Accessible Authentication (Minimum)</h2>
+          <h3>Level AA</h3>
+          <p>
+            It states that users must be able to access authentication methods
+            using only a keyboard. This means that the authentication process
+            should not require the use of a mouse or other pointing device.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>4.1.2 Name, Role, Value</h2>
+          <h3>Level A</h3>
+          <p>
+            The name and role of user components can be understood by
+            technology.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
+
+      <section>
+        <header>
+          <h2>4.1.3 Status Messages</h2>
+          <h3>Level AA</h3>
+          <p>
+            Make sure that all messages indicating success or errors are read
+            out by a screen reader.
+          </p>
+        </header>
+
+        {/* TODO: Add example content here that violates this accessibility rule */}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
